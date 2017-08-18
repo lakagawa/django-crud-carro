7a60a7b76dd172985a77661914608637ed3cf7d4
Lais Kagawa <lalakagawa@hotmail.com>
2017-08-17 22:01:05 -0300

    Checkin 2

--- carro/models.py
+++ carro/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Carro(models.Model):
+    modelo = models.CharField(max_length=200)
+    marca = models.CharField(max_length=200)
+    ano = models.CharField(max_length=4)
+
+    created_date = models.DateTimeField(
+            auto_now_add=True)
+
+    def __str__(self):
+        return self.modelo
