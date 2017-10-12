a82fb82e0b64cc23d75a0cdef50460356b0b383c
Vicente, C <cristiano@live.jp>
2017-10-12 18:27:38 -0300

    Finalizado trabalho \o/

--- carro/urls.py
+++ carro/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^cadastrar_carro/', cadastrar_carro, name='cadastrar_carro'),
-    url(r'^listar_carros/', listar_carro, name='carro_list'),
+    url(r'^$', index, name='index'),
+    url(r'^cadastrar_usuario', cadastrar_usuario, name='cadastrar_usuario'),
+    url(r'^login', do_login, name='login'),
+    url(r'^logout', do_logout, name='logout'),
+    url(r'^cadastrar_carro', cadastrar_carro, name='cadastrar_carro'),
+    url(r'^listar_carros', listar_carro, name='carro_list'),
+    url(r'^editar_carro/(?P<pk>[0-9]+)', editar_carro, name='editar_carro'),
+    url(r'^remover_carro/(?P<pk>[0-9]+)', remover_carro, name='remover_carro'),
 ]
--- carro/views.py
+++ carro/views.py
@@ -1,16 +1,70 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.forms import ModelForm
-from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login, logout
+from django.forms import ModelForm, PasswordInput
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
+from django.contrib.auth.models import User
 from .models import *
+
+
+def index(request, template_name='index.html'):
+    return render(request, template_name)
 
 
 class CarroForm(ModelForm):
     class Meta:
         model = Carro
         fields = ['modelo', 'marca', 'ano']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        password = PasswordInput()
+        widgets = {'password': PasswordInput()}
+
+
+class LoginForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        password = PasswordInput()
+        widgets = {'password': PasswordInput()}
+
+
+def cadastrar_usuario(request, template_name='user_form.html'):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = User(username=form['username'].value(), email=form['email'].value())
+        user.set_password(form['password'].value())
+        user.save()
+        return redirect('index')
+    return render(request, template_name, {'form': form})
+
+
+def do_login(request, template_name='login.html'):
+    form = LoginForm(request.POST or None)
+    user = authenticate(username=form['username'].value(), password=form['password'].value())
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('index')
+        else:
+            pass
+            # Retorna uma mensagem de erro de 'conta desabilitada' .
+    else:
+        pass
+        # Retorna uma mensagem de erro 'login inválido'.
+    return render(request, template_name, {'form': form})
+
+
+def do_logout(request):
+    logout(request)
+    return redirect('index')
 
 
 def cadastrar_carro(request, template_name='carro_form.html'):
@@ -23,4 +77,31 @@ def cadastrar_carro(request, template_name='carro_form.html'):
 
 def listar_carro(request, template_name="carro_list.html"):
     carro = Carro.objects.all()
-    return render(request, template_name, {'lista': carro})
+    is_authenticated = request.user.is_authenticated()
+    print(request.user.is_authenticated())
+
+    return render(request, template_name, {'lista': carro, 'is_authenticated': is_authenticated})
+
+
+def editar_carro(request, pk, template_name='carro_form.html'):
+    carro = get_object_or_404(Carro, pk=pk)
+    if not request.user.is_authenticated():
+        return redirect('login')
+    if request.method == "POST":
+        form = CarroForm(request.POST, instance=carro)
+        if form.is_valid():
+            form.save()
+            return redirect('carro_list')
+    else:
+        form = CarroForm(instance=carro)
+    return render(request, template_name, {'form': form})
+
+
+def remover_carro(request, pk, template_name='carro_delete.html'):
+    carro = Carro.objects.get(pk=pk)
+    if not request.user.is_authenticated():
+        return redirect('login')
+    if request.method == "POST":
+        carro.delete()
+        return redirect('carro_list')
+    return render(request, template_name, {'carro': carro})
